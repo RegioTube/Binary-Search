9a290736014807f1dbb6e4a3887dcbe95c6633da
Regio <Demicgaming666@gmail.com>
2025-11-10 09:45:09 +0000

    Add files via upload

---
+++ Binary search.py
@@ -0,0 +1,24 @@
+nlist = [12, 24, 34, 67, 70, 75, 81, 89, 99]
+search_term = 75
+found = False
+
+first = 0
+last = len(nlist)-1
+
+while (found == False and first<=last):
+
+    mid = (first+last)//2
+
+    if search_term == nlist[mid]:
+        found = True
+
+    else:
+        if (search_term > nlist[mid]):
+            first = mid + 1
+
+        elif (search_term < nlist[mid]):
+            last = mid - 1
+if (found):
+    print("Data found!")
+else:
+    print("No Object Found!")
