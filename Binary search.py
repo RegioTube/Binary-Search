983bf56603db66cb2a1d24f8a59d9bd4ba9bf8e9
Regio <Demicgaming666@gmail.com>
2025-11-11 13:30:31 +0000

    Update Binary search.py

--- Binary search.py
+++ Binary search.py
@@ -1,6 +1,7 @@
 nlist = [12, 24, 34, 67, 70, 75, 81, 89, 99]
 search_term = 75
 found = False
+x = 0
 
 first = 0
 last = len(nlist)-1
@@ -18,7 +19,10 @@ while (found == False and first<=last):
 
         elif (search_term < nlist[mid]):
             last = mid - 1
+    x = x + 1
 if (found):
     print("Data found!")
+    print("Operation count:" x)
 else:
     print("No Object Found!")
+
